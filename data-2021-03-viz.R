b2022aed4c192e4c1cd138909581e54a80b8010a
Toby Dylan Hocking <tdhock5@gmail.com>
2021-05-21 15:24:01 -0400

    viz median + iqr

--- data-2021-03-viz.R
+++ data-2021-03-viz.R
@@ -5,7 +5,7 @@ data.dt <- data.table::fread("data-2021-03.csv")
 some.cells <- 1:10
 some.data <- data.dt[cell %in% some.cells]
 
-ggplot()+
+gg <- ggplot()+
   geom_point(aes(
     current.nA, resistance.ohms),
     data=some.data)+
@@ -13,7 +13,7 @@ ggplot()+
   theme(panel.spacing=grid::unit(0, "lines"))+
   facet_grid(degrees.C ~ cell, labeller=label_both)
 
-ggplot()+
+gg <- ggplot()+
   geom_point(aes(
     sqrt(current.nA), log(resistance.ohms)),
     data=some.data)+
@@ -21,7 +21,7 @@ ggplot()+
   theme(panel.spacing=grid::unit(0, "lines"))+
   facet_grid(degrees.C ~ cell, labeller=label_both)
 
-ggplot()+
+gg <- ggplot()+
   geom_point(aes(
     sqrt(current.nA), log(resistance.ohms), fill=degrees.C),
     shape=21,
@@ -31,7 +31,7 @@ ggplot()+
   theme(panel.spacing=grid::unit(0, "lines"))+
   facet_grid(. ~ cell, labeller=label_both)
 
-ggplot()+
+gg <- ggplot()+
   scale_color_gradient(low="white", high="red")+
   geom_boxplot(aes(
     sqrt(current.nA), log(resistance.ohms),
@@ -42,7 +42,7 @@ ggplot()+
   theme(panel.spacing=grid::unit(0, "lines"))+
   facet_grid(. ~ cell, labeller=label_both)
 
-ggplot()+
+gg <- ggplot()+
   scale_fill_gradient(low="white", high="red")+
   geom_violin(aes(
     sqrt(current.nA), log(resistance.ohms),
@@ -78,5 +78,118 @@ gg <- ggplot()+
   theme(panel.spacing=grid::unit(0, "lines"))+
   facet_grid(. ~ cell, labeller=label_both)
 png("data-2021-03-viz-10-cells.png", width=10, height=5, res=200, units="in")
+print(gg)
+dev.off()
+
+data.stats <- data.dt[, .(
+  q75=quantile(resistance.ohms, 0.75),
+  median=median(resistance.ohms),
+  q25=quantile(resistance.ohms, 0.25),
+  measurements=.N
+), by=.(cell, current.nA, degrees.C)]
+data.stats[, iqr := q75-q25]
+table(data.stats$measurements)
+data.stats[measurements < max(measurements)]#same as in missing fig.
+
+zeros <- data.table(count=nrow(data.stats[iqr==0]))
+gg <- ggplot()+
+  geom_point(aes(
+    0, count),
+    data=zeros)+
+  geom_histogram(aes(
+    iqr),
+    data=data.stats)+
+  scale_x_log10("Inter-quartile range over measurements for a (cell,current,temperature)")+
+  ggtitle("Range of measurements is bimodal, with some zeros")
+png("data-2021-03-viz-iqr-hist.png", width=6, height=5, res=200, units="in")
+print(gg)
+dev.off()
+
+lim <- 10^c(0, 7)
+gg <- ggplot()+
+  theme_bw()+
+  theme(panel.spacing=grid::unit(0, "lines"))+
+  geom_abline(aes(
+    slope=slope,intercept=intercept),
+    data=data.table(slope=1,intercept=0),
+    color="grey")+
+  coord_equal(xlim=lim, ylim=lim)+
+  scale_fill_manual(values=c("0"="blue", "23"="white", "80"="red"))+
+  geom_point(aes(
+    iqr, median, fill=factor(degrees.C)),
+    shape=21,
+    data=data.stats)+
+  scale_x_log10("Inter-quartile range of measurements for a (cell,current,temperature)")+
+  scale_y_log10("Median of measurements for a (cell,current,temperature)")+
+  ggtitle("Two clusters of (cell,current,temperature) data")
+png("data-2021-03-viz-iqr-median-scatter.png", width=6, height=6, res=200, units="in")
+print(gg)
+dev.off()
+
+gg <- ggplot()+
+  theme_bw()+
+  theme(panel.spacing=grid::unit(0, "lines"))+
+  geom_abline(aes(
+    slope=slope,intercept=intercept),
+    data=data.table(slope=1,intercept=0),
+    color="grey")+
+  coord_equal(xlim=lim, ylim=lim)+
+  facet_grid(. ~ degrees.C, labeller=label_both)+
+  geom_point(aes(
+    iqr, median),
+    shape=21,
+    data=data.stats)+
+  scale_x_log10("Inter-quartile range of measurements for a (cell,current,temperature)")+
+  scale_y_log10("Median of measurements for a (cell,current,temperature)")+
+  ggtitle("Two clusters of (cell,current,temperature) data")
+png("data-2021-03-viz-iqr-median-scatter-panels.png", width=10, height=4, res=200, units="in")
+print(gg)
+dev.off()
+
+cell.temp.ranges <- data.stats[, .(
+  max=max(median),
+  min=min(median),
+  measurements=sum(measurements)
+), by=.(cell, degrees.C)]
+gg <- ggplot()+
+  theme_bw()+
+  theme(panel.spacing=grid::unit(0, "lines"))+
+  facet_grid(degrees.C ~ ., labeller=label_both)+
+  geom_segment(aes(
+    cell, min,
+    color=factor(measurements),
+    xend=cell, yend=max),
+    data=cell.temp.ranges)+
+  geom_point(aes(
+    cell, (min+max)/2,
+    color=factor(measurements)),
+    shape=1,
+    data=cell.temp.ranges[min==max])+
+  scale_y_log10("Median measurement values for each (cell,temperature)
+Segment = range over all currents, Point = only value when min=max")+
+  scale_color_manual("N",values=c("400"="grey50", "384"="green", "8"="violet"))
+png("data-2021-03-viz-iqr-cell-ranges-N.png", width=10, height=5.2, res=200, units="in")
+print(gg)
+dev.off()
+
+cell.temp.ranges[, smallest.upper := min(max), by=cell]
+cell.temp.ranges[, class := ifelse(smallest.upper < 1e5, "small", "large")]
+gg <- ggplot()+
+  theme_bw()+
+  theme(panel.spacing=grid::unit(0, "lines"))+
+  facet_grid(degrees.C ~ ., labeller=label_both)+
+  geom_segment(aes(
+    cell, min,
+    color=class,
+    xend=cell, yend=max),
+    data=cell.temp.ranges)+
+  geom_point(aes(
+    cell, (min+max)/2,
+    color=class),
+    shape=1,
+    data=cell.temp.ranges[min==max])+
+  scale_y_log10("Median measurement values for each (cell,temperature)
+Segment = range over all currents, Point = only value when min=max")
+png("data-2021-03-viz-iqr-cell-ranges-class.png", width=10, height=5.2, res=200, units="in")
 print(gg)
 dev.off()
